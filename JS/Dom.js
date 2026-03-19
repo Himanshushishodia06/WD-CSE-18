7ca5fa5c664da359f42d2106cc068308313fbe60
Himanshushishodia06 <himanshushishodia2007@gmail.com>
2026-03-19 09:21:22 +0530

    style,color,size-change

---
+++ JS/Dom.js
@@ -0,0 +1,7 @@
+function result() {
+    document.getElementById("text").style.color = "red";
+    document.getElementById("text").style.backgroundColor = "yellow";
+    document.getElementById("text").style.fontSize = "50px";
+    document.getElementById("text").style.fontFamily = "italic";
+    document.getElementById("text").style.textAlign = "center";
+}
